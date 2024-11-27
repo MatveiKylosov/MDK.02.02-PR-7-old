0dd896293890ce846c54d4acfdf58c0f93ac0776
matvei <matveikylosov@gmail.com>
2024-11-27 22:23:51 +0500

    Получение информации с сайта. Kylosov

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,25 @@ namespace HttpNewsPAT
     {
         static void Main(string[] args)
         {
+            // Создём запрос для получения данных на странице
+            WebRequest request = WebRequest.Create("http://news.permaviat.ru/main");
+            // Выполняем запрос, записывая результат в переменную response
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            // Выводим статус ответа в консоль
+            Console.WriteLine(response.StatusDescription);
+            // Создаём поток для чтения данных ответа
+            Stream dataStream = response.GetResponseStream();
+            // Инициализируем поток для чтения данных
+            StreamReader reader = new StreamReader(dataStream);
+            // Читаем ответ
+            string responseFromServer = reader.ReadToEnd();
+            // Выодим ответ в консоль
+            Console.WriteLine(responseFromServer);
+            // Закрываем потоки и соединение
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            Console.Read();
         }
     }
 }
